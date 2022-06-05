f78a0fbe3ff92218158eedda51d7adcd710b0e84
bhankymyk <bankole123michael@gmail.com>
2022-06-05 18:51:40 +0100

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(97, 123):
     if char != 'q' and char != 'e':
-        print("{}".format(char + ord("a")), end="")
+        print("{}".format(chr(char)), end="")
