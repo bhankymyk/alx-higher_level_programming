32fe765e54840bb24af2233ab3f77240bf447042
bhankymyk <bankole123michael@gmail.com>
2022-06-05 21:28:09 +0100

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            num = 32
+        else:
+            num = 0
+        print("{}".format(ord(str[i] - num), end=""))
+    print()
