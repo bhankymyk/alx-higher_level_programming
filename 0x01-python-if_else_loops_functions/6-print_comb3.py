c75a77d2ff81ea289c8ae328b7079a3670ad1f0a
bhankymyk <bankole123michael@gmail.com>
2022-06-05 19:42:09 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num in range(100):
     if int(num / 10) != num % 10 and int(num / 10) < num % 10:
         print("{}{}".format(int(num / 10), num % 10, end=""))
-        if (num !=89):
+        if (num != 89):
             print(", ", end="")
 print("")
