51affe82ed7591638103fac237d28a0f26eb4afd
bhankymyk <bankole123michael@gmail.com>
2022-06-22 23:30:00 +0100

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Representation of  a square"""
+
+    def __init__(self, size):
+        """Initiate a new Squar.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
