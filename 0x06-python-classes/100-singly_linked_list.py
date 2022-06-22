f730fe8d6e9b5d36ace048f04455a218e473ad3d
bhankymyk <bankole123michael@gmail.com>
2022-06-23 00:01:53 +0100

    100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Node:
+    """Representation of a square"""
+
+    def data(self):
+        pass
+
+    def next_node(self):
+        pass
+
+    def next_node(self, value):
+        pass
+
+    def __init__(self):
+        pass
