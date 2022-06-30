7d7b7601ab550e6aa3c868b69a829df4a4db14df
bhankymyk <bankole123michael@gmail.com>
2022-06-30 17:06:42 +0100

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -61,5 +62,5 @@ class Rectangle:
         return (rect)
 
     def __del__(self):
-        type(self).number_of_instances -= 1
+        type(self).number_of_instances += 1
         print("Bye rectangle...")
