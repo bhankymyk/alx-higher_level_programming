1a089ea15f5fec6618f2d44b8b5e7f8326290950
bhankymyk <bankole123michael@gmail.com>
2022-07-06 13:42:52 +0100

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 
-""""This module shows an empty class of Base Geometry"""
+""""Contains class BaseGeometry"""
 
 
 class BaseGeometry:
-    """Empty Geometry"""
+    """implementation"""
     def area(self):
         """find area
         Raises:
-         Exception: area not implemented"""
+            Exception: area not implemented"""
     raise Exception("area() is not implemented")
