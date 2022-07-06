156fc170879b1d7df9946414e7455418e5af210d
bhankymyk <bankole123michael@gmail.com>
2022-07-06 15:26:24 +0100

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""It implements a Square object"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Implementation"""
+
+    def __init__(self, size):
+        super().__init__(size, size)
