13ba98ca59ecbd86e72d3896542928a319715dea
bhankymyk <bankole123michael@gmail.com>
2022-07-06 20:31:50 +0100

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module returns the JSON representation of an object"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """"Covert object to json string"""
+    return json.dumbs(my_obj)
