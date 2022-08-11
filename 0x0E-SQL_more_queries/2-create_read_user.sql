c05fb97f65d7714f7ff6cac8dcb9c04770060810
bhankymyk <bankole123michael@gmail.com>
2022-08-11 22:35:33 +0100

    2-create_read_user.sql added

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,8 @@ CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
  IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON * TO 'hbtn_0d_2'
+GRANT SELECT 
+ON 'hbtn_0d_2'
 TO 'user_0d_1'@'localhost';
 IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
