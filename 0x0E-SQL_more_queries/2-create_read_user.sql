f4c4c47bd73c5799d6a32a816e0dac48ddcf1e4c
bhankymyk <bankole123michael@gmail.com>
2022-08-11 22:34:02 +0100

    2-create_read_user.sql added

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,10 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * TO 'user_0d_1'@'localhost';
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+ IDENTIFIED BY 'user_0d_1_pwd';
+GRANT SELECT ON * TO 'hbtn_0d_2'
+TO 'user_0d_1'@'localhost';
+IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
