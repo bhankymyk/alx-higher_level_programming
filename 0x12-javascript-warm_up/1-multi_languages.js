08650d2b21a8c9cd21a6e5e86a199027bd9818a8
bhankymyk <bankole123michael@gmail.com>
2022-09-05 16:38:40 +0100

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let myVar = 'C is fun';
+let myVar1 = 'python is cool';
+let myVar2 = 'JavaScript is amazing';
+
+  console.log(myVar)
+  console.log(myVar1)
+  console.log(myVar2)
+  
