ee20bc239c15ad2b87c16e64113fb4991fb858e2
bhankymyk <bankole123michael@gmail.com>
2022-09-05 19:24:54 +0100

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let multiLanguage= [
+    'C is fun',
+    'Python is cool',
+    'Javascript is amazing'
+];
+for (let i = 0; i < multiLanguage.length; i++) {
+    console.log(multiLanguage[i]);
+}
