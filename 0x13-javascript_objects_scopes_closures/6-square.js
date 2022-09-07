7701fd63566888ae8bc83f3d8c7394b01405d0e9
bhankymyk <bankole123michael@gmail.com>
2022-09-07 22:38:00 +0100

    6-square.js added

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.charPrint();
+    } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
+    }
+  }
+};
