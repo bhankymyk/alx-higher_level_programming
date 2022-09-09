5410074b45d612a18fe05666fe1b369f6319d71e
bhankymyk <bankole123michael@gmail.com>
2022-09-09 22:06:47 +0100

    3-my_safe_filter_states.py added

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""sql injection"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        }
+        )
+        allStates = cur.fetchall()
+
+    if states is not None:
+        for state in allStates:
+            print(state)
