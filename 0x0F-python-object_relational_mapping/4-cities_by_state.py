a00ebd5032119099ca9c985e11b6cc0a1f25c29d
bhankymyk <bankole123michael@gmail.com>
2022-09-09 22:21:07 +0100

    4-cities_by_state.py added

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""sql injection"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
+
+        allStates = cur.fetchall()
+
+    if states is not None:
+        for state in allStates:
+            print(state)
+            
