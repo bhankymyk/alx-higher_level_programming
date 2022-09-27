329b2b3b152899510bf33c9b5d512f764af8a213
bhankymyk <bankole123michael@gmail.com>
2022-09-27 14:12:58 +0100

    3-error_code.py added

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)
