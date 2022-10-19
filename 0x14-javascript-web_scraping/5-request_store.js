ca33ed14418af9693482ddfab62ff414f7f69a12
bhankymyk <bankole123michael@gmail.com>
2022-10-19 14:10:44 +0100

    request

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(process.argv[3], body, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
