74b4008ca5a98417e72fd735234c0e9a7e7ea52a
bhankymyk <bankole123michael@gmail.com>
2022-10-19 14:59:02 +0100

    fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-request(url, function (error, response, body) {
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
   }
-  for (const role of JSON.parse(body).characters) {
-    request(role, function (error, response, body) {
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    request.get(i, function (error, res, body) {
       if (error) {
         console.log(error);
-      } else {
-        console.log(JSON.parse(body).name);
       }
+      const data = JSON.parse(body);
+      console.log(data.name);
     });
   }
 });
