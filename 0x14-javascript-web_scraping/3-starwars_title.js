e56957c4dcf165a4dcc149c698cabc15d0a4ed46
bhankymyk <bankole123michael@gmail.com>
2022-10-19 14:00:08 +0100

    starwar api fetch modified

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request ('https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]),function (err, _res, body) {
+request ('https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]),function (err, response, body) {
         if (err) {
           console.log(err);
         }
