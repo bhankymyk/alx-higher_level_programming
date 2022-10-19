27277e3077144dee0a1263231a787083bfadea00
bhankymyk <bankole123michael@gmail.com>
2022-10-19 14:43:51 +0100

    fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  const completeTasks = {};
-  for (const task of JSON.parse(body)) {
-    if (task.complete === true) {
-      if (task.userId in completeTasks) {
-        completeTasks[task.userId] += 1;
-      } else {
-        completeTasks[task.userId] = 1;
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-  console.log(completeTasks);
 });
